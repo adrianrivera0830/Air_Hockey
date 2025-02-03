1860d4d53a185c21ec6039f960171089eb8d62e6
Adrian Rivera <adrianluna0830@gmail.com>
2025-02-03 16:16:30 -0600

    main files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+#include <SDL.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    // Inicializar SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Error al inicializar SDL: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    std::cout << "SDL2 inicializado correctamente." << std::endl;
+
+    // Limpiar y cerrar SDL antes de salir
+    SDL_Quit();
+
     return 0;
 }
